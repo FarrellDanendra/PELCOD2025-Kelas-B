cd0bb7edbc0a869d1bb4800f46f2cee4166d39d9
FarrellDanendra <farelldrb.1602@gmail.com>
2025-09-13 11:52:26 +0700

    250441100015_FarrellDanendra_Day1

---
+++ 250441100015_FarrellDanendra_Day1/pelcod/day1.py
@@ -0,0 +1,30 @@
+# nama = "Farrell Danendra Rafif Bustomi"
+# nim = 250441100015
+# semester = 1
+
+# print("====BIODATA MAHASISWA====")
+# print("Hallo, nama saya: "+ nama + "\n") 
+# print(f"Nim saya: {nim}\n")
+# print("Saya berkuliah di semester {}".format(semester))
+# \n berfungsi untuk menambahkan baris atau jarak
+
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a % b
+# print("Hasil penjumlahan : ", c)
+# print("Hasil pengurangan : ", d)
+# print("Hasil perkalian : ", e)
+# print("Hasil pembagian : ", f)
+# print("Hasil persenan : ", g)
+
+nama = input("Masukkan nama anda: ")
+umur = input("Masukkan umur anda: ")
+buah = input("Masukkan buah anda: ")
+
+print("Nama saya adalah " + nama)
+print("Umur saya adalah " + umur + " tahun")
+print("Buah favorit saya adalah " + buah)
